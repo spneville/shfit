b8a62766a2d55de0c6f182464de0a1b74458e442
Simon Neville <sneville.sneville@gmail.com>
2020-04-02 13:38:56 -0400

    CMake file added

---
+++ build/CMakeFiles/shfit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shfit.dir/src/genpoints.f90.o"
+  "CMakeFiles/shfit.dir/src/lebedev.f90.o"
+  "CMakeFiles/shfit.dir/src/sphharm.f90.o"
+  "libshfit.dylib"
+  "libshfit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/shfit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/shfit.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,10 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "lebedev.mod"
+  "LEBEDEV.mod"
+  "CMakeFiles/shfit.dir/lebedev.mod.stamp"
+
+  "sphharm.mod"
+  "SPHHARM.mod"
+  "CMakeFiles/shfit.dir/sphharm.mod.stamp"
+  )
